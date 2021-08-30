c912ff3f7780e7c5339fa484dfc6750875a34c48
unknown <amielzxc088@gmail.com>
2021-08-30 22:36:18 +0800

    added some function to articles

--- src/components/_dashboard/blog/AddBlogForm.js
+++ src/components/_dashboard/blog/AddBlogForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { useForm } from 'react-hook-form';
+import Axios from 'axios';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -25,29 +26,58 @@ export default function AddBlogForm() {
     handleSubmit,
     formState: { errors }
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    console.log(data);
+    const image = data.image[0];
+    const title = data.name;
+    const description = data.description;
+
+    const formData = new FormData();
+    formData.append('file', image);
+    formData.append('upload_preset', 'kbusolgc');
+    await Axios.post('https://api.cloudinary.com/v1_1/dy5g3pexw/image/upload', formData).then(
+      (response, err) => {
+        if (!err) {
+          console.log(response.data.secure_url);
+          const obj = {
+            imagePath: response.data.secure_url,
+            articleTitle: title,
+            articleDescription: description,
+            articleDate: `${new Date().getUTCMonth()}/${new Date().getUTCDate()}/${new Date().getUTCFullYear()}`
+          };
+          Axios.post('http://localhost:3001/article/addArticle', obj).then((response) => {
+            console.log(response);
+          });
+        }
+      }
+    );
+  };
   const classes = useStyles();
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={classes.root}>
-      <input type="file" name="picture" accept="image/*" />
+      <input id="image" type="file" name="picture" accept="image/*" {...register('image')} />
       {/* use aria-invalid to indicate field contain error */}
+
       <input
         id="name"
         aria-invalid={errors.name ? 'true' : 'false'}
-        {...register('name', { required: true, maxLength: 30 })}
+        {...register('name', { required: true, maxLength: 10000 })}
         placeholder="Title"
       />
       <input
         id="description"
         aria-invalid={errors.name ? 'true' : 'false'}
-        {...register('desription', { required: true, maxLength: 30 })}
+        {...register('description', { required: true, maxLength: 1000000 })}
         placeholder="Description"
       />
       {/* use role="alert" to announce the error message */}
-      {errors.name && errors.name.type === 'required' && <span role="alert">tite</span>}
+      {errors.name && errors.name.type === 'required' && (
+        <span role="alert">Please fill out the form.</span>
+      )}
       {errors.name && errors.name.type === 'maxLength' && (
         <span role="alert">Max length exceeded</span>
       )}
+      <input type="submit" />
     </form>
   );
 }
